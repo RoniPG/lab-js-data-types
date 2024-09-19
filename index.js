80070417f543f4464883a6012bd2ab3076d8fa47
RoniPG <ronigmn@gmail.com>
2024-09-19 23:05:53 +0200

    Iteration 1.2 | Camel Tail

--- index.js
+++ index.js
@@ -24,10 +24,11 @@ const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
+const result = `${part1.substring(0, part1.length -1)}${part1[part1.length-1].toUpperCase()} ${part2.substring(0, part2.length -1)}${part2[part2.length-1].toUpperCase()}`
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(result);
 
 
 /*******************************************
