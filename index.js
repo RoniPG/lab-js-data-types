3005fc32ceb68b073e144bf1cd9ba4b6b9349b26
RoniPG <ronigmn@gmail.com>
2024-09-19 22:53:52 +0200

    Iteration 1.1 | Tongue Twister

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const s5 = "and";
 
 // Concatenate the string variables into one new string
 
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1}`
 
 // Print out the concatenated string
 
-
+console.log(tongueTwister);
 
 
 /*******************************************
